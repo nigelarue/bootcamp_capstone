c2853daf3881627cf983486131c7147d76706ca4
nigelarue <larue.nigel91@gmail.com>
2023-02-07 16:03:55 -0600

    adding landing page logic and root css styling.

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,16 +1,26 @@
 import React from "react";
+import "./Landing/Landing.css";
+import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
+import App from "../App";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
 
 function Landing() {
+
+
   return (
-    <div className="flex-row space-between">
-      <h3>APP DESCRIPTION DON'T FORGET</h3>
-      <Button onClick={() => window.location.assign("/signup")}>Sign Up</Button>
-      <Button onClick={() => window.location.assign("/login")}>Log In</Button>
+    <div className="style__landing-title flex-row space-between">
+      <h3 className="style__landing-hdr">APP DESCRIPTION DON'T FORGET</h3>
+      <a href="/signup">
+        <Button className="style__landing-btn">Sign Up</Button>
+      </a>
+      <a href="/login">
+        <Button className="style__landing-btn">Log In</Button>
+      </a>
     </div>
   );
 }
+
 
 export default Landing;
